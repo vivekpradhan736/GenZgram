e9ffaae5f8d24b16ecfb3e92efadd3574519f42e
Vivek <vivekpradhan736@gmail.com>
2023-11-18 18:09:21 +0530

    first commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,27 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useUserContext } from "../context/AuthContext";
+
+
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.png"
+            alt="logo"
+            className="hidden xl:block w-1/2 h-screen object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+}
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+
+import { Topbar, Bottombar, LeftSidebar } from "../components/shared";
+
+const RootLayout = () => {
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
+};
+
+export default RootLayout;
